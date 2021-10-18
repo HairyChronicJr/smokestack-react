5f7c6b466897fdc6603205b837b4ba8127b6968d
Chris McConnell <cwmc310@gmail.com>
2021-10-18 16:07:14 -0600

    authentication setup

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const config = require('./config');
 
 const recipeRouter = require('./routes/recipeRouter');
+const usersRouter = require('./routes/users');
+
+const mongoose = require('mongoose');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
 
 const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
 app.use('/api/recipes', recipeRouter);
+app.use('/users', usersRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
